0e49ffd288612399177bb1dde4219bad0f8626d6
Rodrigo Brunale <rodrigo@brunale.com>
2024-03-12 18:05:26 -0300

    feat: Enhance data processing and add Pub/Sub notification
    - Added type hints to all functions to improve code readability and catch potential type-related errors.
    - Added docstrings to all functions to provide clear documentation about their purpose, parameters, and return values.
    - Converted constants to fetch data from environment variables for easier configuration management and flexibility in deployment.
    - Implemented a Pub/Sub notification feature to send a message with the UUID after the last JSON payload has been uploaded to the Google Cloud Storage bucket.
    - Created a new function `publish_notification` to handle the Pub/Sub notification using the `google.cloud.pubsub_v1` library.
    - Updated the `process_webhook_payload` function to call the `publish_notification` function with the UUID after processing the pedidos pesquisa data.
    - Refactored the code to improve readability and maintainability while focusing on data quality and robustness.

--- api_to_gcs/api_to_gcs-gcf.py
+++ api_to_gcs/api_to_gcs-gcf.py
@@ -1,22 +1,25 @@
 import json
 import requests
 import hashlib
+import os
 from datetime import datetime
+
 from google.cloud import storage, secretmanager
+from google.cloud import pubsub_v1
 from tenacity import retry, wait_exponential, stop_after_attempt, retry_if_exception_type
 
-BASE_URL = "https://api.tiny.com.br/api2/"
-FILE_PREFIX = "z316-tiny-api-"
-TARGET_BUCKET_NAME = "z316-tiny-api"
-FOLDER_NAME = "{timestamp}-{dados_id}-{uuid_str}"
-PDV_FILENAME = "{dados_id}-pdv-{timestamp}-{uuid_str}"
-PESQUISA_FILENAME = "{dados_id}-pesquisa-{timestamp}-{uuid_str}"
-PRODUTO_FILENAME = "{dados_id}-produto-{produto_id}-{timestamp}-{uuid_str}"
-SECRET_PATH = "projects/559935551835/secrets/z316-tiny-token-api/versions/latest"
-
-PROJECT_ID = "z316-sales-data-pipeline"
-SOURCE_IDENTIFIER = "google-cloud-function"
-VERSION_CONTROL = "git_comit_id"
+BASE_URL = os.environ['BASE_URL']
+FILE_PREFIX = os.environ['FILE_PREFIX']
+TARGET_BUCKET_NAME = os.environ['TARGET_BUCKET_NAME']
+FOLDER_NAME = os.environ['FOLDER_NAME']
+PDV_FILENAME = os.environ['PDV_FILENAME']
+PESQUISA_FILENAME = os.environ['PESQUISA_FILENAME']
+PRODUTO_FILENAME = os.environ['PRODUTO_FILENAME']
+SECRET_PATH = os.environ['SECRET_PATH']
+PROJECT_ID = os.environ['PROJECT_ID']
+SOURCE_IDENTIFIER = os.environ['SOURCE_IDENTIFIER']
+VERSION_CONTROL = os.environ['VERSION_CONTROL']
+PUBSUB_TOPIC = os.environ['PUBSUB_TOPIC']
 
 storage_client = storage.Client()
 secret_manager_client = secretmanager.SecretManagerServiceClient()
@@ -24,16 +27,36 @@ secret_manager_client = secretmanager.SecretManagerServiceClient()
 class ValidationError(Exception):
     pass
 
+
 class InvalidTokenError(Exception):
     pass
+
 
 class RetryableError(Exception):
     pass
 
-def print_message(message, context=None):
+
+def print_message(message: str, context: Optional[dict] = None) -> None:
+    """
+    Prints a message with optional context information.
+
+    Args:
+        message (str): The message to be printed.
+        context (Optional[dict]): Additional context information. Defaults to None.
+    """
     print(f"{message} - Context: {context}" if context else message)
 
-def get_api_token():
+
+def get_api_token() -> str:
+    """
+    Retrieves the API token from the Secret Manager.
+
+    Returns:
+        str: The API token.
+
+    Raises:
+        Exception: If there is an error accessing the secret.
+    """
     try:
         print_message("Accessing API token from Secret Manager")
         response = secret_manager_client.access_secret_version(request={"name": SECRET_PATH})
@@ -42,8 +65,22 @@ def get_api_token():
         print_message(f"Failed to access secret: {e}")
         raise
 
+
 @retry(wait=wait_exponential(multiplier=2.5, min=30, max=187.5), stop=stop_after_attempt(4), retry=retry_if_exception_type((requests.exceptions.RequestException, RetryableError)))
-def make_api_call(url):
+def make_api_call(url: str) -> dict:
+    """
+    Makes an API call to the specified URL and returns the JSON response.
+
+    Args:
+        url (str): The URL to make the API call to.
+
+    Returns:
+        dict: The JSON response from the API.
+
+    Raises:
+        requests.exceptions.RequestException: If there is an error making the API request.
+        ValidationError: If the JSON payload validation fails.
+    """
     try:
         sanitized_url = url.split('?token=')[0]
         print_message(f"Making API call to: {sanitized_url}")
@@ -61,7 +98,19 @@ def make_api_call(url):
         print_message(f"Payload validation failed: {e}")
         raise
 
-def validate_json_payload(json_data):
+
+def validate_json_payload(json_data: dict) -> None:
+    """
+    Validates the JSON payload received from the API.
+
+    Args:
+        json_data (dict): The JSON payload to validate.
+
+    Raises:
+        ValidationError: If the payload validation fails.
+        InvalidTokenError: If the token is invalid.
+        RetryableError: If a retryable error is encountered.
+    """
     status_processamento = json_data.get('retorno', {}).get('status_processamento')
 
     if status_processamento == '3':
@@ -77,7 +126,21 @@ def validate_json_payload(json_data):
         else:
             raise RetryableError("Error encountered, will attempt retry: " + erro_message)
 
-def read_webhook_payload(bucket_name, file_name):
+
+def read_webhook_payload(bucket_name: str, file_name: str) -> dict:
+    """
+    Reads the webhook payload from the specified Google Cloud Storage bucket and file.
+
+    Args:
+        bucket_name (str): The name of the bucket containing the webhook payload.
+        file_name (str): The name of the file containing the webhook payload.
+
+    Returns:
+        dict: The webhook payload as a dictionary.
+
+    Raises:
+        Exception: If there is an error reading the webhook payload.
+    """
     try:
         bucket = storage_client.bucket(bucket_name)
         blob = bucket.blob(file_name)
@@ -86,7 +149,15 @@ def read_webhook_payload(bucket_name, file_name):
         print_message(f"Failed to read webhook payload: {e}")
         raise
 
-def process_webhook_payload(event, context):
+
+def process_webhook_payload(event: dict, context: Any) -> None:
+    """
+    Processes the webhook payload received from the event.
+
+    Args:
+        event (dict): The event containing the webhook payload.
+        context (Any): The context of the event.
+    """
     print_message("Function execution started", context={'event_id': context.event_id})
     try:
         payload_details = extract_payload_details(event)
@@ -96,12 +167,24 @@ def process_webhook_payload(event, context):
         token = get_api_token()
         pedido_numero = process_pdv_pedido_data(*payload_details, token)
         process_pedidos_pesquisa_data(*payload_details, token, pedido_numero)
+        publish_notification(PUBSUB_TOPIC, payload_details[2])
 
     except Exception as e:
         print_message(f"Function failed: {e}", context={'event_id': context.event_id})
     print_message("Function execution completed successfully", context={'event_id': context.event_id})
 
-def extract_payload_details(event):
+
+def extract_payload_details(event: dict) -> Optional[Tuple[str, str, str]]:
+    """
+    Extracts the payload details from the event.
+
+    Args:
+        event (dict): The event containing the payload details.
+
+    Returns:
+        Optional[Tuple[str, str, str]]: A tuple containing the dados_id, timestamp, and uuid_str,
+                                         or None if dados_id is not found in the webhook payload.
+    """
     file_name = event['name']
     webhook_payload = read_webhook_payload(event['bucket'], file_name)
     dados_id = webhook_payload.get('dados', {}).get('id')
@@ -113,7 +196,20 @@ def extract_payload_details(event):
     parts = file_name.rstrip('.json').split('-')
     return dados_id, parts[-6], '-'.join(parts[-5:])
 
-def process_pdv_pedido_data(dados_id, timestamp, uuid_str, token):
+
+def process_pdv_pedido_data(dados_id: str, timestamp: str, uuid_str: str, token: str) -> str:
+    """
+    Processes the PDV pedido data.
+
+    Args:
+        dados_id (str): The dados_id.
+        timestamp (str): The timestamp.
+        uuid_str (str): The UUID string.
+        token (str): The API token.
+
+    Returns:
+        str: The pedido_numero.
+    """
     folder_path = FOLDER_NAME.format(timestamp=timestamp, dados_id=dados_id, uuid_str=uuid_str)
     pdv_pedido_data = fetch_pdv_pedido_data(dados_id, token)
     pedido_numero = pdv_pedido_data.get('retorno', {}).get('pedido', {}).get('numero')
@@ -136,7 +232,18 @@ def process_pdv_pedido_data(dados_id, timestamp, uuid_str, token):
 
     return pedido_numero
 
-def process_pedidos_pesquisa_data(dados_id, timestamp, uuid_str, token, pedido_numero):
+
+def process_pedidos_pesquisa_data(dados_id: str, timestamp: str, uuid_str: str, token: str, pedido_numero: str) -> None:
+    """
+    Processes the pedidos pesquisa data.
+
+    Args:
+        dados_id (str): The dados_id.
+        timestamp (str): The timestamp.
+        uuid_str (str): The UUID string.
+        token (str): The API token.
+        pedido_numero (str): The pedido_numero.
+    """
     folder_path = FOLDER_NAME.format(timestamp=timestamp, dados_id=dados_id, uuid_str=uuid_str)
     pedidos_data = fetch_pedidos_pesquisa_data(pedido_numero, token)
     store_payload(pedidos_data, PESQUISA_FILENAME.format(dados_id=dados_id, timestamp=timestamp, uuid_str=uuid_str), folder_path, {
@@ -145,19 +252,72 @@ def process_pedidos_pesquisa_data(dados_id, timestamp, uuid_str, token, pedido_n
         'data_type': 'pedidos.pesquisa'
     })
 
-def fetch_pdv_pedido_data(dados_id, token):
+
+def fetch_pdv_pedido_data(dados_id: str, token: str) -> dict:
+    """
+    Fetches the PDV pedido data from the API.
+
+    Args:
+        dados_id (str): The dados_id.
+        token (str): The API token.
+
+    Returns:
+        dict: The PDV pedido data.
+    """
     return make_api_call(f"{BASE_URL}pdv.pedido.obter.php?token={token}&id={dados_id}")
 
-def fetch_produto_data(item_id, token):
+
+def fetch_produto_data(item_id: str, token: str) -> dict:
+    """
+    Fetches the produto data from the API.
+
+    Args:
+        item_id (str): The item_id.
+        token (str): The API token.
+
+    Returns:
+        dict: The produto data.
+    """
     return make_api_call(f"{BASE_URL}produto.obter.php?token={token}&id={item_id}&formato=JSON")
 
-def fetch_pedidos_pesquisa_data(pedido_numero, token):
+
+def fetch_pedidos_pesquisa_data(pedido_numero: str, token: str) -> dict:
+    """
+    Fetches the pedidos pesquisa data from the API.
+
+    Args:
+        pedido_numero (str): The pedido_numero.
+        token (str): The API token.
+
+    Returns:
+        dict: The pedidos pesquisa data.
+    """
     return make_api_call(f"{BASE_URL}pedidos.pesquisa.php?token={token}&numero={pedido_numero}&formato=JSON")
 
-def generate_checksum(data):
+
+def generate_checksum(data: dict) -> str:
+    """
+    Generates a checksum for the given data.
+
+    Args:
+        data (dict): The data to generate the checksum for.
+
+    Returns:
+        str: The generated checksum.
+    """
     return hashlib.md5(json.dumps(data, sort_keys=True).encode('utf-8')).hexdigest()
 
-def store_payload(data, filename_template, folder_path, metadata):
+
+def store_payload(data: dict, filename_template: str, folder_path: str, metadata: dict) -> None:
+    """
+    Stores the payload in Google Cloud Storage.
+
+    Args:
+        data (dict): The payload data.
+        filename_template (str): The template for the filename.
+        folder_path (str): The folder path in Google Cloud Storage.
+        metadata (dict): Additional metadata to store with the payload.
+    """
     file_path = f"{folder_path}/{FILE_PREFIX}{filename_template}.json"
     print_message(f"Storing payload in GCS at: {file_path}")
 
@@ -186,3 +346,20 @@ def store_payload(data, filename_template, folder_path, metadata):
         print_message(f"Payload stored with metadata: {full_metadata}")
     except Exception as e:
         print_message(f"Failed to store payload in GCS: {e}")
+
+
+def publish_notification(topic_path: str, message: str) -> None:
+    """
+    Publishes a notification message to the specified Pub/Sub topic.
+
+    Args:
+        topic_path (str): The path of the Pub/Sub topic.
+        message (str): The message to publish.
+    """
+    try:
+        topic_path = publisher.topic_path(PROJECT_ID, topic_path)
+        future = publisher.publish(topic_path, data=message.encode('utf-8'))
+        print_message(f"Notification published to {topic_path} with message: {message}")
+        future.result()
+    except Exception as e:
+        print_message(f"Failed to publish notification: {e}")
